13e696363f6cde7ce77f1e172acc710a788cc77b
Jumeb <bricejume@gmail.com>
2022-06-28 18:05:43 +0100

    Fonts, Navigation bar, Header

--- src/components/NavBar/TopNavBar.component.js
+++ src/components/NavBar/TopNavBar.component.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FiChevronRight, FiMenu } from 'react-icons/fi';
+import { IoPersonCircle } from 'react-icons/io5';
 
 import styles from './NavBar.module.css';
-import { Search } from '..';
-import { menu, profile } from '../../resources/Icons';
 
 const TopNavBar = props => {
+    const [scrollY, setScrollY] = useState(0);
+    console.log(scrollY)
+
+    const logit = () => {
+        setScrollY(window.pageYOffset);
+    }
+
+    useEffect(() => {
+        const watchScroll = () => {
+            window.addEventListener("scroll", logit);
+        }
+        watchScroll();
+        return () => {
+            window.removeEventListener("scroll", logit);
+        };
+    }, [scrollY]);
+
     return (
-        <div className={`${styles.mainWrapper}`}>
-            <h3 className={`${styles.logoTitle}`}>Logo</h3>
-            <Search />
-            <button className="outline-none">
-                <h2 className={`${styles.expertCTA}`}>Expert:in werden</h2>
+        <div className={`${styles.mainWrapper} ${scrollY >= 140 ? 'bg-neutral-white shadow-md' : 'bg-transparent'}`}>
+            <h3 className={`${styles.logoTitle} ${scrollY >= 140 ? 'text-primary-lila' : 'text-neutral-white'}`}>skillflash</h3>
+            <button className={`${styles.navButton}`}>
+                <FiChevronRight className={`${styles.navButtonIcon} ${scrollY >= 140 ? 'text-primary-orange' : 'text-neutral-white'}`} />
+                <h2 className={`${styles.expertCTA} ${scrollY >= 140 ? 'from-secondary-pink via-secondary-pink to-primary-orange' : 'from-neutral-white to-neutral-white'}`}>Expert:in werden</h2>
             </button>
-            <div className={`${styles.profileNavWrapper} bg-primary-orange shadow-neutral-black`}>
-                <button className={`${styles.profileButton}`}>
-                    <img alt="icon" src={menu} />
-                </button>
-                <button className={`${styles.profileButton}`}>
-                    <img alt="icon" src={profile} />
-                </button>
-            </div>
+            <button className={`${styles.navButton}`}>
+                <FiChevronRight className={`${styles.navButtonIcon} ${scrollY >= 140 ? 'text-primary-orange' : 'text-neutral-white'}`} />
+                <h2 className={`${styles.expertCTA} ${scrollY >= 140 ? 'from-secondary-pink via-secondary-pink to-primary-orange' : 'from-neutral-white to-neutral-white'}`}>Enterprise</h2>
+            </button>
+            <button className={`${styles.profileNavWrapper} ${scrollY >= 140 ? 'bg-neutral-white' : 'bg-transparent'}`}>
+                <FiMenu className={`${scrollY >= 140 ? 'mx-1 text-2xl text-primary-orange' : 'mx-1 text-2xl text-neutral-white'}`} />
+                <IoPersonCircle className={`${scrollY >= 140 ? 'mx-1 text-2xl text-primary-orange' : 'mx-1 text-2xl text-neutral-white'}`} />
+            </button>
         </div>
     )
 }
