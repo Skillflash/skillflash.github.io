a6387c12e2bee991582875fa7aaefa570c81de2b
Jumeb <bricejume@gmail.com>
2022-07-09 11:14:38 +0100

    Updated the responsive design

--- src/sections/ProjectHeader/ProjectHeader.section.js
+++ src/sections/ProjectHeader/ProjectHeader.section.js
@@ -10,7 +10,7 @@ const ProjectHeader = props => {
     const { scrollY } = props;
     const [activeSkills, setActiveSkills] = useState(['Projektmanagement']);
     const [selectedSkills, setSelectedSkills] = useState(['Projektmanagement']);
-    const [skills, setSkills] = useState(['Klassich', 'Hauptskill', 'Subskill', 'Subskill', 'Subskill', 'Subskill', 'Subskill', 'Subskill', 'Subskill', 'Subskill', 'Subskill', 'Subskill', 'Subskill', 'Subskill']);
+    const [skills, setSkills] = useState(['Klassich', 'Hauptskill', 'Subsksill', 'Subskidll', 'Sudbskill', 'Subskgill', 'Suebskill', 'Subshkill', 'Subskrill', 'Subskibll', 'Subskiall', 'Subskijll', 'Subskiltl', 'Subiskill']);
     const [range, setRange] = useState(20);
 
     const showSize = () => {
@@ -29,19 +29,72 @@ const ProjectHeader = props => {
         };
     }, []);
 
+    const SetActiveSkills = data => {
+        let _skills = [...activeSkills];
+        let skillsIndex = _skills.findIndex(s => s === data);
+        if (skillsIndex < 0) {
+            _skills.push(data);
+        } else {
+            _skills.splice(skillsIndex, 1);
+        }
+        setActiveSkills([..._skills])
+    }
+
+    const SetSelectedSkills = data => {
+        let _skills = [...selectedSkills];
+        let skillsIndex = _skills.findIndex(s => s === data);
+        let _allSkills = [...skills];
+        let removeIndex = _allSkills.findIndex(a => a === data)
+        if (skillsIndex < 0) {
+            _skills.push(data);
+        } else {
+            _skills.splice(skillsIndex, 1);
+        }
+        if (removeIndex < 0) {
+            _allSkills.unshift(data)
+        } else {
+            _allSkills.splice(removeIndex, 1);
+        }
+        setSkills([..._allSkills]);
+        setSelectedSkills([..._skills])
+    }
+
+    const RemoveSelectedSkill = data => {
+        let _allSkills = [...skills];
+        let removeIndex = _allSkills.findIndex(a => a === data);
+    }
+
+    const SetSelectedActiveSkills = (data, event) => {
+        SetSelectedSkills(data);
+        SetActiveSkills(data);
+        event.stopPropagation()
+    }
+
     return (
         <HeaderWrapper scrollY={scrollY}>
             <div className={`${styles.projectWrapper}`}>
                 {selectedSkills.map((selectedSkill, index) =>
-                    <button key={index} className={`text-neutral-white ${styles.projectButton} bg-neutral-black`}>
+                    <button
+                        onClick={() => SetActiveSkills(selectedSkill)}
+                        key={index}
+                        className={`
+                        text-neutral-white
+                        ${styles.projectButton}
+                        ${activeSkills.findIndex(a => a === selectedSkill) === -1 && 'bg-opacity-70'}
+                        bg-neutral-black`}
+                    >
                         <h6 className={`${styles.projectButtonText}`}>{selectedSkill}</h6>
-                        <IoCloseCircleOutline className={`${styles.projectButtonIcon}`} />
+                        <IoCloseCircleOutline onClick={(event) => SetSelectedActiveSkills(selectedSkill, event)} className={`${styles.projectButtonIcon}`} />
                     </button>
                 )}
             </div>
             <div className={`${styles.projectSkillsWrapper}`}>
                 {skills.slice(0,range).map((skill, index) =>
-                    <button key={index} className={`text-neutral-black ${styles.projectSkillButton} bg-neutral-white border-neutral-black`}>
+                    <button
+                        key={index}
+                        onClick={event => SetSelectedActiveSkills(skill,event)}
+                        className={`text-neutral-black ${styles.projectSkillButton} bg-neutral-white border-neutral-black`}
+                    >
                         <h6 className={`${styles.projectSkillButtonText}`}>{skill}</h6>
                     </button>
                 )}
