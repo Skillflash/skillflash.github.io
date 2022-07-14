326a79216851caf79511233039c374b49a0752c1
HerbertMbangzieu <herbertmbangzieu@gmail.com>
2022-07-14 17:46:19 +0100

    about section

---
+++ src/dummy/experts.js
@@ -0,0 +1,25 @@
+import { avatar7, avatar13, avatar14, avatar5 } from "../resources/Images";
+
+const experts = [
+    {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        experience: '4',
+        domain: ['Hauptskill'],
+        address: undefined,
+        skills: ['Subskill','Subskill',],
+        image: avatar7,
+    },
+    {
+        firstName: 'Antonia',
+        lastName: 'Sensen',
+        experience: '6',
+        domain: ['ProjektManagement'],
+        address: 'Hidelsheim',
+        skills: ['Klassisch','Agil', 'Hybrid'],
+        image: avatar14,
+    },
+
+];
+
+export default experts;
