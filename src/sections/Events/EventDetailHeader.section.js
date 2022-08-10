b6fc849053c1c30249c6378868b9928544d6bbc1
xennia316 <xennia316@gmail.com>
2022-08-10 09:41:38 +0100

    added  and used props

--- src/sections/Events/EventDetailHeader.section.js
+++ src/sections/Events/EventDetailHeader.section.js
@@ -6,34 +6,53 @@ import styles from './Events.module.css';
 import { EventDetailsCard, EventWrapper } from '../../components';
 import { avatar12, avatar13, avatar16, avatar17 } from '../../resources/Images';
 
-const EventDetailHeader = props => {
-    const { scrollY } = props;
+const EventDetailHeader = (props) => {
+  const { scrollY, expert } = props;
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const GoBack = () => {
-        navigate(-1)
-    }
+  const GoBack = () => {
+    navigate(-1);
+  };
 
-    return (
-        <EventWrapper styling=''>
-            <div className={styles.projectBackWrapper}>
-                <button onClick={() => GoBack()} className={`${styles.projectBack} ${scrollY >= 200 ? 'border-primary-orange' : 'border-neutral-white'}`}>
-                    <IoArrowBack className={`${styles.projectBackIcon} ${scrollY >= 200 ? 'text-primary-orange' : 'text-neutral-white'}`} />
-                </button>
-            </div>
-            <div className={`${styles.eventDetails}`}>
-                <EventDetailsCard />
-                <div className={`${styles.eventAuthor}`}>
-                    <div className={`${styles.eventAuthorDetails}`}>
-                        <img src={avatar13} alt='Author' className={`${styles.eventAuthorImage} border-neutral-white`} />
-                        <h4 className={`${styles.eventAuthorName} text-neutral-white`}>Paul Mertens</h4>
-                        <p className={`${styles.eventAuthorText} text-neutral-white`}>..organsiert dieses Event.</p>
-                    </div>
-                </div>
-            </div>
-        </EventWrapper>
-    )
-}
+  return (
+    <EventWrapper styling="">
+      <div className={styles.projectBackWrapper}>
+        <button
+          onClick={() => GoBack()}
+          className={`${styles.projectBack} ${
+            scrollY >= 200 ? "border-primary-orange" : "border-neutral-white"
+          }`}
+        >
+          <IoArrowBack
+            className={`${styles.projectBackIcon} ${
+              scrollY >= 200 ? "text-primary-orange" : "text-neutral-white"
+            }`}
+          />
+        </button>
+      </div>
+      <div className={`${styles.eventDetails}`}>
+        <EventDetailsCard />
+        <div className={`${styles.eventAuthor}`}>
+          <div className={`${styles.eventAuthorDetails}`}>
+            <img
+              src={
+                "http://localhost:8055/assets/c1982e98-5b6d-4327-807a-2561002029d7?access_token=mytoken&width=60&height=60&quality=80"
+              }
+              alt={expert.data && expert.data[0].author.profileImage.type}
+              className={`${styles.eventAuthorImage} border-neutral-white`}
+            />
+            <h4 className={`${styles.eventAuthorName} text-neutral-white`}>
+              {expert.data && expert.data[0].author.firstName}
+            </h4>
+            <p className={`${styles.eventAuthorText} text-neutral-white`}>
+              {expert.data && expert.data[0].author.shortBio}
+            </p>
+          </div>
+        </div>
+      </div>
+    </EventWrapper>
+  );
+};
 
 export default EventDetailHeader;
