21c53456354f1d476f5b47789c225a69db118bfe
xennia316 <xennia316@gmail.com>
2022-08-11 21:15:54 +0100

    check if event exists then map

--- src/sections/Events/Events.section.js
+++ src/sections/Events/Events.section.js
@@ -7,18 +7,21 @@ import styles from './Events.module.css';
 const Events = props => {
     const { events } = props; 
     return (
-        <section className={`${styles.mainWrapper} bg-neutral-white`}>
-            {events.loading ?
-                <div className='mt-20'>
-                    <LoadingIndicator size={2} color={theme.SECONDARY_PINK} />
-                </div>
-             :
-                <div className={`${styles.eventsCards}`}>
-                {events.data.map((event, index) => <ProjectCard event={event} key={index} />)}
-            </div>
-            }
-        </section>
-    )
+      <section className={`${styles.mainWrapper} bg-neutral-white`}>
+        {events.loading ? (
+          <div className="mt-20">
+            <LoadingIndicator size={2} color={theme.SECONDARY_PINK} />
+          </div>
+        ) : (
+          <div className={`${styles.eventsCards}`}>
+            {events.data &&
+              events.data.map((event, index) => (
+                <ProjectCard event={event} key={index} />
+              ))}
+          </div>
+        )}
+      </section>
+    );
 }
 
 export default Events;
